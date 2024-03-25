7cd48b26fef91e0333e5cfe34de833e2860d5463
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-03-26 04:49:06 +0900

    [feat] ヘッダーとドラッグ機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { motion } from "framer-motion";
+
 export default function App() {
 	return (
 		<div className="flex justify-center items-center flex-col space-y-7">
 			<div className="h-[100vh] w-[100vw] flex justify-center items-center">
-				<h1 className="text-[9vw]">I'm Takayama Haruto.</h1>
+				<motion.div
+					drag
+					dragConstraints={{
+						top: -200,
+						left: -200,
+						right: 200,
+						bottom: 200,
+					}}
+					dragSnapToOrigin={true}
+				>
+					<img
+						className="pointer-events-none"
+						src="/public/I'm Takayama haruto.svg"
+						alt="I'm Takayama Haruto"
+					/>
+				</motion.div>
 			</div>
 			<div className="h-[60vh] w-[70vw] flex justify-center items-center bg-gradient-to-r from-red-400/20 to-blue-600/60; rounded-xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
 				<p className="text-7xl text-slate-300">hello</p>
@@ -28,30 +45,6 @@ export default function App() {
 			<div className="h-[60vh] w-[70vw] flex justify-center items-center bg-gradient-to-r from-red-400/20 to-blue-600/60; rounded-xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
 				<p className="text-7xl text-slate-300">hello</p>
 			</div>
-
-			{/* <motion.div
-				transition={{
-					duration: 2,
-					ease: "easeInOut",
-					times: [0, 0.2, 0.5, 0.8, 1],
-					repeat: Number.POSITIVE_INFINITY,
-					repeatDelay: 1,
-				}}
-				drag
-				dragConstraints={{
-					top: -200,
-					left: -200,
-					right: 200,
-					bottom: 200,
-				}}
-				className="relative z-10 w-[170px] h-[170px] rounded-full  bg-sky-500"
-			>
-				<img
-					className="pointer-events-none"
-					src="https://media.istockphoto.com/id/185284489/photo/orange.jpg?s=612x612&w=0&k=20&c=m4EXknC74i2aYWCbjxbzZ6EtRaJkdSJNtekh4m1PspE="
-					alt="orange"
-				/>
-			</motion.div> */}
 		</div>
 	);
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,37 +1,79 @@
+import { motion } from "framer-motion";
+
 const Header = () => {
 	return (
 		<div className="fixed top-0 right-0 left-0 py-4 px-6 z-10">
 			<header className="flex justify-between">
 				<div className="space-x-6">
-					<button
-						type="button"
+					<motion.button
 						className="text-white text-2xl font-bold"
 						onClick={() => console.log("Home")}
+						whileHover={{ scale: 1.2, textDecoration: "underline" }}
+						whileTap={{ scale: 0.9 }}
+						transition={{ type: "spring", stiffness: 400, damping: 17 }}
+						drag
+						dragSnapToOrigin={true}
+						dragConstraints={{
+							top: -5,
+							left: -5,
+							right: 5,
+							bottom: 50,
+						}}
 					>
-						Home
-					</button>
-					<button
-						type="button"
+						<p>Home</p>
+					</motion.button>
+					<motion.button
 						className="text-white text-2xl font-bold"
 						onClick={() => console.log("Career")}
+						whileHover={{ scale: 1.2, textDecoration: "underline" }}
+						whileTap={{ scale: 0.9 }}
+						transition={{ type: "spring", stiffness: 400, damping: 17 }}
+						drag
+						dragSnapToOrigin={true}
+						dragConstraints={{
+							top: -5,
+							left: -5,
+							right: 5,
+							bottom: 50,
+						}}
 					>
-						Career
-					</button>
-					<button
-						type="button"
+						<p>Career</p>
+					</motion.button>
+					<motion.button
 						className="text-white text-2xl font-bold"
 						onClick={() => console.log("activity")}
+						whileHover={{ scale: 1.2, textDecoration: "underline" }}
+						whileTap={{ scale: 0.9 }}
+						transition={{ type: "spring", stiffness: 400, damping: 17 }}
+						drag
+						dragSnapToOrigin={true}
+						dragConstraints={{
+							top: -5,
+							left: -5,
+							right: 5,
+							bottom: 50,
+						}}
 					>
-						Activity
-					</button>
+						<p>Activity</p>
+					</motion.button>
 				</div>
-				<button
-					type="button"
+				<motion.button
 					className="text-white text-2xl font-bold"
 					onClick={() => console.log("activity")}
+					whileHover={{ scale: 1.2, textDecoration: "underline" }}
+					whileTap={{ scale: 0.9 }}
+					transition={{ type: "spring", stiffness: 400, damping: 17 }}
+					drag
+					dragSnapToOrigin={true}
+					dragConstraints={{
+						top: -5,
+						left: -5,
+						right: 5,
+						bottom: 50,
+					}}
 				>
-					Contacts
-				</button>
+					<p>Contacts</p>
+				</motion.button>
 			</header>
 		</div>
 	);
