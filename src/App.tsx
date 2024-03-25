28f31976a2d15f67ccafb96781a3e59eaea926ca
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-03-26 05:24:16 +0900

    [feat] ページ内ジャンプ機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { motion } from "framer-motion";
+import { Element } from "react-scroll";
 
 export default function App() {
 	return (
-		<div className="flex justify-center items-center flex-col space-y-7">
+		<div className="flex justify-center items-center flex-col space-y-7 pb-96">
 			<div className="h-[100vh] w-[100vw] flex justify-center items-center">
 				<motion.div
 					drag
@@ -21,30 +22,26 @@ export default function App() {
 					/>
 				</motion.div>
 			</div>
-			<div className="h-[60vh] w-[70vw] flex justify-center items-center bg-gradient-to-r from-red-400/20 to-blue-600/60; rounded-xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
-				<p className="text-7xl text-slate-300">hello</p>
-			</div>
-			<div className="h-[60vh] w-[70vw] flex justify-center items-center bg-gradient-to-r from-red-400/20 to-blue-600/60; rounded-xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
-				<p className="text-7xl text-slate-300">hello</p>
-			</div>
-			<div className="h-[60vh] w-[70vw] flex justify-center items-center bg-gradient-to-r from-red-400/20 to-blue-600/60; rounded-xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
-				<p className="text-7xl text-slate-300">hello</p>
-			</div>
-			<div className="h-[60vh] w-[70vw] flex justify-center items-center bg-gradient-to-r from-red-400/20 to-blue-600/60; rounded-xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
-				<p className="text-7xl text-slate-300">hello</p>
-			</div>
-			<div className="h-[60vh] w-[70vw] flex justify-center items-center bg-gradient-to-r from-red-400/20 to-blue-600/60; rounded-xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
-				<p className="text-7xl text-slate-300">hello</p>
-			</div>
-			<div className="h-[60vh] w-[70vw] flex justify-center items-center bg-gradient-to-r from-red-400/20 to-blue-600/60; rounded-xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
-				<p className="text-7xl text-slate-300">hello</p>
-			</div>
-			<div className="h-[60vh] w-[70vw] flex justify-center items-center bg-gradient-to-r from-red-400/20 to-blue-600/60; rounded-xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
-				<p className="text-7xl text-slate-300">hello</p>
-			</div>
-			<div className="h-[60vh] w-[70vw] flex justify-center items-center bg-gradient-to-r from-red-400/20 to-blue-600/60; rounded-xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
-				<p className="text-7xl text-slate-300">hello</p>
-			</div>
+			<Element name="Home">
+				<div className="h-[60vh] w-[70vw] flex justify-center items-center bg-gradient-to-r from-red-400/20 to-blue-600/60; rounded-xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
+					<p className="text-7xl text-slate-300">Home</p>
+				</div>
+			</Element>
+			<Element name="Career">
+				<div className="h-[60vh] w-[70vw] flex justify-center items-center bg-gradient-to-r from-red-400/20 to-blue-600/60; rounded-xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
+					<p className="text-7xl text-slate-300">Career</p>
+				</div>
+			</Element>
+			<Element name="Activity">
+				<div className="h-[60vh] w-[70vw] flex justify-center items-center bg-gradient-to-r from-red-400/20 to-blue-600/60; rounded-xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
+					<p className="text-7xl text-slate-300">Activity</p>
+				</div>
+			</Element>
+			<Element name="Contacts">
+				<div className="h-[60vh] w-[70vw] flex justify-center items-center bg-gradient-to-r from-red-400/20 to-blue-600/60; rounded-xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
+					<p className="text-7xl text-slate-300">Contacts</p>
+				</div>
+			</Element>
 		</div>
 	);
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,44 +1,70 @@
 import { motion } from "framer-motion";
+import { Link } from "react-scroll";
 
 const Header = () => {
 	return (
 		<div className="fixed top-0 right-0 left-0 py-4 px-6 z-10">
 			<header className="flex justify-between">
 				<div className="space-x-6">
-					<motion.button
-						className="text-white text-2xl font-bold"
-						onClick={() => console.log("Home")}
-						whileHover={{ scale: 1.2, textDecoration: "underline" }}
-						whileTap={{ scale: 0.9 }}
-						transition={{ type: "spring", stiffness: 400, damping: 17 }}
-						drag
-						dragSnapToOrigin={true}
-						dragConstraints={{
-							top: -5,
-							left: -5,
-							right: 5,
-							bottom: 50,
-						}}
-					>
-						<p>Home</p>
-					</motion.button>
-					<motion.button
-						className="text-white text-2xl font-bold"
-						onClick={() => console.log("Career")}
-						whileHover={{ scale: 1.2, textDecoration: "underline" }}
-						whileTap={{ scale: 0.9 }}
-						transition={{ type: "spring", stiffness: 400, damping: 17 }}
-						drag
-						dragSnapToOrigin={true}
-						dragConstraints={{
-							top: -5,
-							left: -5,
-							right: 5,
-							bottom: 50,
-						}}
-					>
-						<p>Career</p>
-					</motion.button>
+					<Link to="Home" smooth={true} offset={-70} duration={500}>
+						<motion.button
+							className="text-white text-2xl font-bold"
+							onClick={() => console.log("Home")}
+							whileHover={{ scale: 1.2, textDecoration: "underline" }}
+							whileTap={{ scale: 0.9 }}
+							transition={{ type: "spring", stiffness: 400, damping: 17 }}
+							drag
+							dragSnapToOrigin={true}
+							dragConstraints={{
+								top: -5,
+								left: -5,
+								right: 5,
+								bottom: 50,
+							}}
+						>
+							<p>Home</p>
+						</motion.button>
+					</Link>
+					<Link to="Career" smooth={true} offset={-70} duration={500}>
+						<motion.button
+							className="text-white text-2xl font-bold"
+							onClick={() => console.log("Career")}
+							whileHover={{ scale: 1.2, textDecoration: "underline" }}
+							whileTap={{ scale: 0.9 }}
+							transition={{ type: "spring", stiffness: 400, damping: 17 }}
+							drag
+							dragSnapToOrigin={true}
+							dragConstraints={{
+								top: -5,
+								left: -5,
+								right: 5,
+								bottom: 50,
+							}}
+						>
+							<p>Career</p>
+						</motion.button>
+					</Link>
+					<Link to="Activity" smooth={true} offset={-70} duration={500}>
+						<motion.button
+							className="text-white text-2xl font-bold"
+							onClick={() => console.log("activity")}
+							whileHover={{ scale: 1.2, textDecoration: "underline" }}
+							whileTap={{ scale: 0.9 }}
+							transition={{ type: "spring", stiffness: 400, damping: 17 }}
+							drag
+							dragSnapToOrigin={true}
+							dragConstraints={{
+								top: -5,
+								left: -5,
+								right: 5,
+								bottom: 50,
+							}}
+						>
+							<p>Activity</p>
+						</motion.button>
+					</Link>
+				</div>
+				<Link to="Contacts" smooth={true} offset={-70} duration={500}>
 					<motion.button
 						className="text-white text-2xl font-bold"
 						onClick={() => console.log("activity")}
@@ -54,26 +80,9 @@ const Header = () => {
 							bottom: 50,
 						}}
 					>
-						<p>Activity</p>
+						<p>Contacts</p>
 					</motion.button>
-				</div>
-				<motion.button
-					className="text-white text-2xl font-bold"
-					onClick={() => console.log("activity")}
-					whileHover={{ scale: 1.2, textDecoration: "underline" }}
-					whileTap={{ scale: 0.9 }}
-					transition={{ type: "spring", stiffness: 400, damping: 17 }}
-					drag
-					dragSnapToOrigin={true}
-					dragConstraints={{
-						top: -5,
-						left: -5,
-						right: 5,
-						bottom: 50,
-					}}
-				>
-					<p>Contacts</p>
-				</motion.button>
+				</Link>
 			</header>
 		</div>
 	);
