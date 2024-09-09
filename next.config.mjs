515086a426589eb02d6cf0978b23b38c47627e56
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-09-09 22:28:26 +0900

    [refactor] next.config.mjsの設定を更新

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    webpack(config) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        minSize: 20000,
-        maxSize: 70000,
-      };
-      return config;
-    },
-  };
-  
-  module.exports = nextConfig;
+	/* config options here */
+};
+module.exports = nextConfig;
