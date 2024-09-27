24f87d47fdcc2229a0cb53ace1793e78c44b5003
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-09-27 09:21:55 +0900

    [update] importの集約

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import Footer from "./components/layout/Footer/index.tsx";
-import Header from "./components/layout/Header/index.tsx";
-import ParticleBackground from "./components/common/Particle/index.tsx";
-import ScrollProgressIndicator from "./components/common/ScrollProgressIndicator/index.tsx";
-import SvgObject from "./components/common/SvgObject/index.tsx";
+import { Footer, Header, ParticleBackground, ScrollProgressIndicator, SvgObject } from "./components";
 import Home from "./pages/home.tsx";
 
 export default function App() {
-    return (
-      <>
-      <Header />
-      <Home />
-      <Footer />
+	return (
+		<>
+			<Header />
+			<Home />
+			<Footer />
 			<SvgObject />
 			<ScrollProgressIndicator />
 			<ParticleBackground />
-      </>
-    );
+		</>
+	);
 }
