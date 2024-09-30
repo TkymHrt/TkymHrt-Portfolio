140b7457a03f3286924d45d5b47fea77ffb8f844
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-09-30 19:07:24 +0900

    [fix] フォントをページ全体に適用

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="ja" className={`${notoSansJP.variable} antialiased`}>
+		<html lang="ja" className={`${notoSansJP.variable} ${notoSansJP.className} antialiased`}>
 			<body>
 				{children}
 				<SvgObject />
